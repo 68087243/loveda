3195db6a14222bf994fe33246ceb351c0c9f01c4
yihong <841058801@qq.com>
2017-06-03 19:21:25 +0800

    xsss

--- Common/Conf/local/config.php
+++ Common/Conf/local/config.php
@@ -9,7 +9,7 @@ return array(
     'DB_PORT' => '3306',
     'DB_PREFIX' => 't_', //表前缀
     'SHOW_PAGE_TRACE' => false, //显示调试信息
-    'DOMAIN' => 'http://www.lovehou.com',
+    'DOMAIN' => 'http://www.adsvip.cn',
      /* 日志 */
     'LOG_RECORD' => true, // 开启日志记录
     'LOG_LEVEL' => 'ERR,NOTIC,INFO,DEBUG,SQL', // 只记录EMERG ALERT CRIT ERR 错误
@@ -17,9 +17,9 @@ return array(
     'IS_COMPRESS_HTML'                  =>false,  //是否压缩界面
     // 静态资源
     'TMPL_PARSE_STRING'=>array(
-        '__PUBLIC__'=>'http://www.lovehou.com/Public',
-        '__STATIC__'=> 'http://www.lovehou.com/Public/static',
-        '__UPLOAD__'=> 'http://www.lovehou.com/Public/upload',
+        '__PUBLIC__'=>'http://www.adsvip.cn/Public',
+        '__STATIC__'=> 'http://www.adsvip.cn/Public/static',
+        '__UPLOAD__'=> 'http://www.adsvip.cn/Public/upload',
     ),
 );
 ?>
